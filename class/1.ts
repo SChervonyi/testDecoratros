8ae2240720d27ec15b574480896ffa83c884dcf3
Sergii Chervonyi <Bekyts@gmail.com>
2018-02-25 18:06:40 +0200

    clean up

--- class/1.ts
+++ class/1.ts
@@ -1,32 +1,32 @@
-function Helmet<TFunc extends { new(...args: any[]): {} }>(constructor: TFunc): TFunc {
-    return class extends constructor implements SmartDog {
-        say(): string {
-            return "Hi, I'm Snaffles";
-        }
-        thinking(): void {
-            console.log("Where's my bone?");
-        }
-    }
-}
+// function Helmet<TFunc extends { new(...args: any[]): {} }>(constructor: TFunc): TFunc {
+//     return class extends constructor implements SmartDog {
+//         say(): string {
+//             return "Hi, I'm Snaffles";
+//         }
+//         thinking(): void {
+//             console.log("Where's my bone?");
+//         }
+//     }
+// }
 
-@Helmet
-class Snaffles {
-    private name: string = "Snaffles";
-    say() {
-        return "Gaf!";
-    }
-}
+// @Helmet
+// class Snaffles {
+//     private name: string = "Snaffles";
+//     say() {
+//         return "Gaf!";
+//     }
+// }
 
-interface SmartDog {
-    thinking(): void;
-}
+// interface SmartDog {
+//     thinking(): void;
+// }
 
-const snaffles = new Snaffles() as Snaffles & SmartDog;
-console.log(snaffles.say());
-if(snaffles.thinking) { 
-    snaffles.thinking(); 
-}
+// const snaffles = new Snaffles() as Snaffles & SmartDog;
+// console.log(snaffles.say());
+// if(snaffles.thinking) { 
+//     snaffles.thinking(); 
+// }
 
 
 
-//             // return "Hi, I'm " + (<any>this).name;
+// //             // return "Hi, I'm " + (<any>this).name;
--- class/2.ts
+++ class/2.ts
@@ -1,33 +1,30 @@
-// function HelmetFactory(name: string) {
-//     return function Helmet<TFunc extends { new(...args: any[]): {} }>(constructor: TFunc): TFunc | void {
-//         return class extends constructor implements SmartDog {
-//             say(): string {
-//                 return "Hi, I'm " + name;
-//             }
-//             thinking(): void {
-//                 console.log("Where's my bone?");
-//             }
-//         }
-//     }
-// }
+function HelmetFactory(name: string) {
+    return function Helmet<TFunc extends { new(...args: any[]): {} }>(constructor: TFunc): TFunc | void {
+        return class extends constructor implements SmartDog {
+            say(): string {
+                return "Hi, I'm " + name;
+            }
+            thinking(): void {
+                console.log("Where's my bone?");
+            }
+        }
+    }
+}
 
-// @HelmetFactory("Snaffles")
-// class Snaffles {
-//     private name: string = "Snaffles";
-//     constructor() {
-//     }
-//     say() {
-//         return "Gaf!";
-//     }
-// }
+@HelmetFactory("Snaffles")
+class Snaffles {
+    say() {
+        return "Gaf!";
+    }
+}
 
-// interface SmartDog {
-//     say(): string;
-//     thinking(): void;
-// }
+interface SmartDog {
+    say(): string;
+    thinking(): void;
+}
 
-// const snaffles = new Snaffles() as Snaffles & SmartDog;
-// console.log(snaffles.say());
-// if(snaffles.thinking) { 
-//     snaffles.thinking(); 
-// }
+const snaffles = new Snaffles() as Snaffles & SmartDog;
+console.log(snaffles.say());
+if(snaffles.thinking) { 
+    snaffles.thinking(); 
+}
